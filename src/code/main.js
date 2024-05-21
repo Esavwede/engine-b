e099e74d27a14f8376bc7cbdfa1abf21d6e6a6ed
ogagaoghene emmanuel <esavwede84@gmail.com>
2024-05-21 13:54:12 +0100

    added core game loop

--- src/code/main.js
+++ src/code/main.js
@@ -1,3 +1,40 @@
 
 
-console.log("hello world")
+
+const canvas = document.getElementById("canvas")
+canvas.width = 640
+canvas.height = 480 
+
+const ctx = canvas.getContext("2d") 
+
+
+// Presets 
+ctx.fillStyle = "blue"
+ctx.strokeStyle = "orange" 
+
+
+//  Game Loop 
+
+var lastAnimationTime = 0 
+var currentAnimationTime = 0 
+var dt = 0 
+
+
+function loopy(ms)
+{
+    requestAnimationFrame( loopy )
+
+    // Delta Time 
+    currentAnimationTime = ms / 1000 
+    dt = currentAnimationTime - lastAnimationTime
+    lastAnimationTime = currentAnimationTime 
+
+
+    console.log( dt ) 
+
+
+}
+
+
+
+requestAnimationFrame( loopy )
